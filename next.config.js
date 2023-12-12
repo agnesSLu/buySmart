2caaab5979aa5dc7a88683d135b441cd489f502c
Agnes(Shihua) Lu <shihualu99@gmail.com>
2023-12-12 03:09:53 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_URL: "https://buysmart-backend.onrender.com",
+    API_URL: "https://buy-smart-backend.vercel.app",
   },
   images: {
       domains: ["m.media-amazon.com"],
